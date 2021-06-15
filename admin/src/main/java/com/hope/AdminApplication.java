5a7922dcf8a17363fbfbf6a6dc4c314e4a0dee26
java-aodeng <java@aodeng.cc>
2021-06-15 15:15:03 +0800

    第三方授权登录

--- admin/src/main/java/com/hope/AdminApplication.java
+++ admin/src/main/java/com/hope/AdminApplication.java
@@ -17,6 +17,6 @@ public class AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
-        LOGGER.info("[填写项目名称]admin服务启动成功！温馨提示：代码千万行，注释第一行，命名不规范，同事泪两行");
+        LOGGER.info("[项目名称]admin服务启动成功！\n温馨提示：代码千万行，注释第一行，命名不规范，同事泪两行");
     }
 }
---
+++ admin/src/main/java/com/hope/controller/base/OauthController.java
@@ -0,0 +1,62 @@
+package com.hope.controller.base;
+
+
+import cn.hutool.json.JSONUtil;
+import com.hope.utils.AjaxResult;
+import com.xkcoding.justauth.AuthRequestFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @className: OauthController
+ * @Description: TODO 第三方授权登录
+ * @version: v1.0.0
+ * @author: 低调小熊猫
+ * @date: 14:06 21-6-15
+ */
+@RestController
+@RequestMapping("/oauth")
+@Slf4j
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class OauthController {
+
+    private final AuthRequestFactory factory;
+
+    @GetMapping
+    public List<String> list() {
+        return factory.oauthList();
+    }
+
+    @GetMapping("/login/{type}")
+    public void login(@PathVariable String type, HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = factory.get(type);
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @RequestMapping("/{type}/callback")
+    public AuthResponse login(@PathVariable String type, AuthCallback callback) {
+        AuthRequest authRequest = factory.get(type);
+        AuthResponse response = authRequest.login(callback);
+        log.info("【response】= {}", JSONUtil.toJsonStr(response));
+        return response;
+    }
+
+    @RequestMapping("/demo")
+    public AjaxResult demo() {
+        return AjaxResult.success("Gitee","http://aodeng.cn1.utools.club/oauth/login/gitee");
+    }
+
+}
